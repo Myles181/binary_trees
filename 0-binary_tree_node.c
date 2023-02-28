a57e258b13e63f7532ab00966ac63d156cd163be
Myles2005 <cyrile450@gmail.com>
2023-02-28 12:49:33 -0800

    adding if statements

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -8,10 +8,12 @@
  */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-	binary_tree_t *node;
+	binary_tree_t *node = malloc(sizeof(binary_tree_t));
 
-	node = malloc(sizeof(binary_tree_t));
-
+	if (parent == NULL || node == NULL)
+	{
+		return NULL;
+	}
 	node->n = value;
 	node->parent = parent;
 	node->left = NULL;
