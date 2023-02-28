271c8d6f429755aed19e32c0d61f7c1d89d356b6
Myles2005 <cyrile450@gmail.com>
2023-02-28 12:45:01 -0800

    binary trees

---
+++ 3-binary_tree_delete.c
@@ -0,0 +1,15 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_delete - To delete a tree
+ * @tree: The root of the tree to be deleted
+ * Return: nothing
+ */
+void binary_tree_delete(binary_tree_t *tree)
+{
+	if (tree == NULL)
+	{
+		free(tree);
+	}
+	free(tree);
+}
