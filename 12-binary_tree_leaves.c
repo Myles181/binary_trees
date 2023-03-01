183588c3d8dae134221200e11a6b976e665ce0e2
Myles2005 <cyrile450@gmail.com>
2023-02-28 18:15:58 -0800

    betty

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -6,12 +6,15 @@
  */
 size_t binary_tree_leaves(const binary_tree_t *tree)
 {
+	size_t leaves;
+
 	if (tree == NULL)
 		return (0);
 
 	if (tree->left == NULL && tree->right == NULL)
 		return (1);
 
-	return binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right);
+	leaves = binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right);
+	return (leaves);
 }
 
