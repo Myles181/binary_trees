63a416af35f01d776cd86c89218b0c94ab80e8db
Myles2005 <cyrile450@gmail.com>
2023-03-02 13:32:45 -0800

    binary tree insert modifications

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -8,9 +8,13 @@
  */
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
-	binary_tree_t *node = malloc(sizeof(binary_tree_t));
+	binary_tree_t *node;
 
-	if (parent == NULL || node == NULL)
+	if (parent == NULL)
+		return (NULL);
+	
+	node = malloc(sizeof(binary_tree_t));
+	if (!node)
 		return (NULL);
 
 	node->n = value;
